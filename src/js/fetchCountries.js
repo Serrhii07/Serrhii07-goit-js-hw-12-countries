b3cc001351da013b99da4add24254d843dc174a4
Serrhii07 <serge070989@gmail.com>
2020-05-03 13:11:58 +0300

    fixed fetchCountries fn

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,13 +8,12 @@ import countryInfoTpl from '../templates/countryInfo.hbs';
 function fetchCountries() {
   const countryName = refs.input.value;
 
+  clearMarkup();
+
   fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
     .then(response => response.json())
     .then(data => {
-      data.length;
-
       if (data.length > 10) {
-        clearMarkup();
         PNotify.error({
           text: 'Too many matches found. Please enter a more specific query!',
           delay: 2000,
@@ -24,7 +23,6 @@ function fetchCountries() {
       } else if (data.length === 1) {
         updateCountryInfo(data);
       } else if (data.status == '404') {
-        clearMarkup();
         PNotify.info({
           text: 'No matches found. Please specify query!',
           delay: 2000,
@@ -32,22 +30,20 @@ function fetchCountries() {
       }
 
       function updateCountriesList(data) {
-        clearMarkup();
         const markup = countriesListTpl(data);
         refs.countriesList.innerHTML = markup;
       }
 
       function updateCountryInfo(data) {
-        clearMarkup();
         const updateCountryMarkup = countryInfoTpl(data);
         refs.countryOutput.innerHTML = updateCountryMarkup;
       }
-
-      function clearMarkup() {
-        refs.countriesList.innerHTML = '';
-        refs.countryOutput.innerHTML = '';
-      }
     });
+
+  function clearMarkup() {
+    refs.countriesList.innerHTML = '';
+    refs.countryOutput.innerHTML = '';
+  }
 }
 
 export default fetchCountries;
